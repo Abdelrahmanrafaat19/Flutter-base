aa1560bda6b598ac48612278d98038d15fcb56ea
Abdelrahman Raafat Shoaib <93776528+Abdelrahmanrafaat19@users.noreply.github.com>
2025-04-07 20:15:08 +0200

    Add EndPoint of Search By name Update firstname and last name add arrow back and text in Search Screen

--- lib/main.dart
+++ lib/main.dart
@@ -149,7 +149,7 @@ class MyApp extends ConsumerWidget {
       GoRoute(
         path: splashScreenRoute,
         builder: (BuildContext context, GoRouterState state) =>
-             SignUpScreen(),
+            const SplashScreen(),
       ),
       GoRoute(
         path: mainScreenRoute,
